a0f397be899aa2a91fd012f6fcaaeb6057f2f0f5
akhmelev <akhmelev@gmail.com>
2025-10-17 21:24:27 +0300

    Lesson01 Values

--- lesson01/src/main/java/com/javarush/led/lesson01/config/ApplicationProperties.java
+++ lesson01/src/main/java/com/javarush/led/lesson01/config/ApplicationProperties.java
@@ -1,17 +1,8 @@
 package com.javarush.led.lesson01.config;
 
-import lombok.SneakyThrows;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
-import java.io.FileReader;
-import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Properties;
 
 @Slf4j
@@ -24,15 +15,16 @@ public class ApplicationProperties extends Properties {
     public static final String HIBERNATE_CONNECTION_DRIVER_CLASS = "hibernate.connection.driver_class";
     public static final String ENV_EXPRESSION = "\\$\\{[A-Z_]+:.+}";
 
-    @SneakyThrows
-    public ApplicationProperties() {
-        this.load(new FileReader(CLASSES_ROOT + "/application.properties"));
-
+    public ApplicationProperties(
+            String url,
+            String username,
+            String password,
+            String driverClass
+    ) {
+        this.put(HIBERNATE_CONNECTION_URL, url);
+        this.put(HIBERNATE_CONNECTION_USERNAME, username);
+        this.put(HIBERNATE_CONNECTION_PASSWORD, password);
+        this.put(HIBERNATE_CONNECTION_DRIVER_CLASS, driverClass);
     }
 
-    //any runtime
-    public final static Path CLASSES_ROOT = Paths.get(URI.create(
-            Objects.requireNonNull(
-                    ApplicationProperties.class.getResource("/")
-            ).toString()));
 }
--- lesson01/src/main/java/com/javarush/led/lesson01/config/Config.java
+++ lesson01/src/main/java/com/javarush/led/lesson01/config/Config.java
@@ -2,17 +2,27 @@ package com.javarush.led.lesson01.config;
 
 import com.javarush.led.lesson01.repository.UserRepository;
 import com.javarush.led.lesson01.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import static com.javarush.led.lesson01.config.ApplicationProperties.*;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 public class Config {
 
     @Bean
-    public ApplicationProperties applicationProperties() {
-        return new ApplicationProperties();
+    public ApplicationProperties applicationProperties(
+            @Value("${" + HIBERNATE_CONNECTION_URL + "}") String url,
+            @Value("${" + HIBERNATE_CONNECTION_USERNAME + "}") String username,
+            @Value("${" + HIBERNATE_CONNECTION_PASSWORD + "}") String password,
+            @Value("${" + HIBERNATE_CONNECTION_DRIVER_CLASS + "}") String driverClass
+    ) {
+        return new ApplicationProperties(
+                url, username, password, driverClass
+        );
     }
 
     @Bean
